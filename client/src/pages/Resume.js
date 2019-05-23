e18bb4bcda2a586c09802f513011258ce7b4a22d
cfern052 <cfern052@gmail.com>
2019-05-23 18:44:06 -0400

    repush of final update

--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
-import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
-import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
@@ -10,6 +8,7 @@ import JobForm from "../components/jobForm";
 import JobWrapper from "../components/jobWrapper"
 import JobCard from "../components/jobCard"
 import '../components/Form/form.css'
+import ResumeCard from "../components/resumeCard"
 
 class Resume extends Component {
     state = {
