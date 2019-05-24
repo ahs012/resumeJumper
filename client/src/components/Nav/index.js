42aee4584e6040cb6bef6d274726c5eb7746cc72
cfern052 <cfern052@gmail.com>
2019-05-23 20:33:16 -0400

    fixed logout button and clear local data

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import "./nav.css";
 
 
-
+function clearData(){localStorage.clear()}
 
 
 const Nav = props => (
@@ -17,7 +17,7 @@ const Nav = props => (
           <li><a href="/Login">Member Login |</a></li>
           <li><a href="/resume">Resume |</a></li>
           <li><a href="/profile">Profile |</a></li>
-          <li><a href="/">Logout</a></li>
+          <li><a href="/" onClick={clearData}>Logout</a></li>
         </ul>
       </div>
     </nav>
